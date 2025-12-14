725bd03c2c1972176953575299c8f66f16c5363b
Sasha <q.dev.null@gmail.com>
2025-12-14 06:33:12 +0100

    Fix the tests

--- tests/test_anonimizes.py
+++ tests/test_anonimizes.py
@@ -1,10 +1,16 @@
 import cv2
+import numpy as np
+import pytest
 
 from streamit.anonymize import draw_bbox_xywh
 
 
+@pytest.fixture
+def frame() -> np.ndarray:
+    return cv2.imread("man-standing.jpg")
+
+
 def test_paints_face(frame):
-    frame = cv2.imread("man-standing.avif")
     frame = draw_bbox_xywh(frame, (328, 80, 94, 138))
     # frame = paint_face(frame, bbox)
     cv2.imshow("frame", frame)
