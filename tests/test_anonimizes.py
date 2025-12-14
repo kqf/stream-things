037d0a2177f5a402e50dabcadab36f6732a5bcdd
kqf <q.dev.null@gmail.com>
2025-12-14 06:36:30 +0100

    Improve the tests (#31)
    * Update the anonimization
    
    * Fix the anonimization
    
    * Fix the requirements
    
    * Fix the anonimization
    
    * Ignore more
    
    * Fix the tests
    
    * Fix the anonimizations

--- tests/test_anonimizes.py
+++ tests/test_anonimizes.py
@@ -1,11 +1,17 @@
 import cv2
+import numpy as np
+import pytest
 
 from streamit.anonymize import draw_bbox_xywh
 
 
+@pytest.fixture
+def frame() -> np.ndarray:
+    return cv2.imread("man-standing.jpg")
+
+
 def test_paints_face(frame):
-    frame = cv2.imread("man-standing.avif")
-    frame = draw_bbox_xywh(frame, (328, 80, 94, 138))
+    frame = draw_bbox_xywh(frame, (207, 28, 70, 88))
     # frame = paint_face(frame, bbox)
     cv2.imshow("frame", frame)
     cv2.waitKey()
