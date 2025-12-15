1c71fb6e63002de0ff2b187bad7bd1c3cfc72866
Sasha <q.dev.null@gmail.com>
2025-12-15 20:44:36 +0100

    Add the pattern

--- tests/test_anonimizes.py
+++ tests/test_anonimizes.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import pytest
 
-from streamit.anonymize import draw_bbox_xywh
+from streamit.anonymize import draw_bbox_xywh, paint_face
 
 
 @pytest.fixture
@@ -10,9 +10,15 @@ def frame() -> np.ndarray:
     return cv2.imread("man-standing.jpg")
 
 
-def test_paints_face(frame):
+@pytest.fixture
+def pattern() -> np.ndarray:
+    return np.random.randint(0, 255, (512 * 512)).reshape(512, 512, 1)
+
+
+def test_paints_face(frame, pattern):
+    bbox = (207, 28, 70, 88)
     frame = draw_bbox_xywh(frame, (207, 28, 70, 88))
-    # frame = paint_face(frame, bbox) ~
+    frame = paint_face(frame, bbox, pattern=pattern)
     cv2.imshow("frame", frame)
     cv2.waitKey()
     cv2.destroyAllWindows()
